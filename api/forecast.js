c8c52354a85f867cf0658f0bfb4552e3a5ec5849
PiyushKhurana <khuranapiyush27@gmail.com>
2019-08-09 22:08:32 +0530

    object destructing & property shorthand

--- api/forecast.js
+++ api/forecast.js
@@ -10,16 +10,16 @@ const forecast = (latitude,longitude,callback) => {
 
     const url = `https://api.darksky.net/forecast/d09b1ae6d76b73ee88d3638f10d3a203/${latitude},${longitude}?units=si`;
 
-    request({ url:url, json:true },(error, response) => {
+    request({ url, json:true },(error, { body }) => {
 
         if (error){
             callback('Unable to connect to forecast service',undefined);
         }
-        else if(response.body.error){
+        else if(body.error){
             callback('Unable to find location ,Try  Another',undefined);
         }
         else{
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.');
         }
     });
 }
--- api/geocode.js
+++ api/geocode.js
@@ -10,19 +10,19 @@ const geocode = (address,callback) => {
 
     url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicXR5OTkyNyIsImEiOiJjanhyZTExdHYwNzJ6M25tajA0dWhiejN5In0.TAuSNln5JlRFoRWoYUDxDQ&limit=1`;
 
-    request({ url:url, json:true },(error,response) => {
+    request({ url:url, json:true },(error, { body }) => {
 
         if(error){
             callback('Unable to connect to geocoding service',undefined);
         }
-        else if(response.body.features.length === 0){
+        else if(body.features.length === 0){
             callback('Unable to find location ,Try  Another',undefined);
         }
         else {
             callback(undefined,{
-                latitude:response.body.features[0].center[1],
-                longitude:response.body.features[0].center[0],
-                location:response.body.features[0].place_name
+                latitude:body.features[0].center[1],
+                longitude:body.features[0].center[0],
+                location:body.features[0].place_name
             });
         }
     })
